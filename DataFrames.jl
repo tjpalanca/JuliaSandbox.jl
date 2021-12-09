9ae1b343be48f7b9a6216d398db9972dc0b4debb
TJ Palanca <mail@tjpalanca.com>
2021-12-09 08:29:05 +0000

    Bang

--- DataFrames.jl
+++ DataFrames.jl
@@ -134,7 +134,7 @@ subset(df, :x => x -> iseven.(x), skipmissing=true) # use skipmissing keyword ar
 # prefer to use subset and subset! instead of filter and filter! because it's more compatible 
 # with other data frame methods.
 
-# Selecting columns (dplyr::select)
+# Selecting columns (dplyr::select), unlike in R this is completely swappable with transform
 df = DataFrame(x1=[1, 2], x2=[3, 4], y=[5, 6])
 select(df, :x1)
 select(df, Not(:x1))
@@ -143,6 +143,64 @@ select(df, :x1 => :boom)
 select(df, :x1, :x2 => (x -> x .- minimum(x)) => :x2)
 select(df, :x2, :x2 => ByRow(sqrt)) # ByRow allows transformation by row (kinda like dplyr::mutate_at)
 select(df, AsTable(:) => ByRow(extrema) => [:lo, :hi]) #can return multiple numbers by using AsTable
+select!(df, Between(:x1, :x2)) # in place selection
 
+# Transform (dplyr::mutate) 
+# For mutate it's select => function => output symbol which is refreshingly interesting
+df = DataFrame(x1=[1, 2], x2=[3, 4], y=[5, 6])
+transform(df, All() => +)
+transform(df, [:x2, :x1] => ((x, y) -> (x .+ y).^2) => :x3)
 
-# DataFrameRow
+# Summarise (dplyr::summarise)
+df = DataFrame(A=1:4, B=["M", "F", "F", "M"])
+describe(df)
+describe(df[!, [:A]])
+df = DataFrame(A=1:4, B=4.0:-1.0:1.0)
+combine(df, names(df) .=> sum)
+
+# Constructors is always copied by default 
+x = [1, 2, 3]
+df = DataFrame(x = x)
+df.x = [3, 2, 1]
+df
+x
+
+# Broadcasting assignment should always be done 
+df = DataFrame(A = 1:4)
+df[:, 1] .= 1
+df
+
+# Column Selectors (Not, Between, Cols, and All)
+german = copy(german_ref)
+german[:, Cols("Age", Between("Sex", "Job"))]
+
+# Transformations 
+using Statistics
+# How to specify the transformations:
+# [A] source_column => transformation => target_column_name (transform)
+# [B] source_column => transformation (column names automatically generated)
+# [C] source_column => target_column_name (renaming)
+# [D] source_column 
+
+# Combine: transformation is applied to all the rows and then return value is assigned/collapsed
+combine(german, :Age => mean => :mean_age)
+
+# Select: transformation to all rows but assigned to each row 
+select(german, :Age => mean => :mean_age)
+
+# Combine: you can supply various different transformations
+combine(german, :Age => mean => :mean_age, :Housing => unique => :housing)
+combine(german, :Age, :Housing => unique => :Housing) # will error, not broadcast compatible
+
+# Select: use broadcasting to apply element-wise
+select(german, :Sex => (x -> uppercase.(x)) => :Sex)
+# Select: alternatively use ByRow to apply element-wise
+select(german, :Sex => ByRow(uppercase) => :SEX)
+
+# Rearrange to the front of the data frame
+select(german, :Sex, :)
+
+# More than one column makes them position arguments
+select(german, :Age, :Job, [:Age, :Job] => (+) => :res)
+
+# TODO: Full Documentation here: https://dataframes.juliadata.org/stable/man/getting_started/
